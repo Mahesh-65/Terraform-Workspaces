25ea6154b9f2cbf0bc95165a47f199a67adf3132
Mahesh Dineshkumar(UST <307420@ust.com>
2026-06-06 17:26:19 +0530

    changes

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "akashterraformstate"
+    resource_group_name  = "Terraform"
+    storage_account_name = "mahitfws"
     container_name       = "terraformstate"
     key                  = "terraform.tfstate"
   }
